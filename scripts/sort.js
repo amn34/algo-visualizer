71efdeb12d881ac60d1c0af3f8773346b5116c57
Anthony <nguyenanthony80@gmail.com>
2021-03-21 12:14:37 -0700

    added timer

--- scripts/sort.js
+++ scripts/sort.js
@@ -1,6 +1,7 @@
 const max = 300;
 const baseArr = [];
 const n = 100;
+const delay = 1;
 
 function generateArray() {
     for(let i = 0; i < n; i++) {
@@ -14,23 +15,26 @@ function getRandomInt(max) {
 
 
 async function bubblesort(arr) {
-    const name = 'Bubble Sort O(n^2)'
+    const name = 'Bubble Sort O(n²)'
     const canvas = document.getElementById("bubblesort");
+    const ctx = canvas.getContext('2d', { alpha: false });
+    const startTime = Date.now();
     let numSwaps = 0
     let swappedIndex = 0;
+
     for(let i = 0 ; i < arr.length; i++) {
         for(let j = 0 ; j < arr.length - i - 1; j++) {
             if (arr[j] > arr[j+1]) {
                 [arr[j], arr[j+1]] = [arr[j+1], arr[j]];
-                numSwaps++;
+                numSwaps++
                 swappedIndex = j
-
-                display(canvas, arr, swappedIndex, numSwaps, name)
-                await new Promise(r => setTimeout(r, 3))
+                display(canvas, ctx, arr, swappedIndex, numSwaps, Date.now() - startTime, name)
+                await new Promise(r => setTimeout(r, delay))
             }
         }
     }
-    console.log(arr);
+    const endTime = Date.now();
+    console.log("Bubble Sort: " + getSeconds((endTime - startTime) - (numSwaps * delay)))
 }
 
 //async function mergersort() {}
@@ -47,12 +51,15 @@ async function countingsort() {}
 async function combsort() {}
 
 async function gnomesort(arr) {
-    const name = 'Gnome Sort O(n^2)'
+    const name = 'Gnome Sort O(n²)'
     const canvas = document.getElementById("gnomesort");
-
-    let i = 0;
+    const ctx = canvas.getContext('2d', { alpha: false });
+    const startTime = Date.now();
     let numSwaps = 0;
     let swappedIndex = 0;
+
+    let i = 0;
+
     while (i < arr.length) {
         if (i == 0) {
             i += 1;
@@ -65,22 +72,19 @@ async function gnomesort(arr) {
             i -= 1;
             numSwaps++;
             swappedIndex = i
-
-            display(canvas, arr, swappedIndex, numSwaps, name)
-            await new Promise(r => setTimeout(r, 3))
+            display(canvas, ctx, arr, swappedIndex, numSwaps, Date.now() - startTime, name)
+            await new Promise(r => setTimeout(r, delay))
         }
-
-
     }
-    console.log(arr);
+    const endTime = Date.now();
+    console.log("Gnome Sort: " + getSeconds((endTime - startTime) - (numSwaps * delay)))
 }
 
 
 async function bogosort(arr) {}
 
-function display(canvas, arr, swappedIndex, numSwaps, name) {
-    const ctx = canvas.getContext('2d');
-    console.log(ctx);
+function display(canvas, ctx, arr, swappedIndex, numSwaps, timeElapsed, name) {
+
     ctx.fillStyle = 'black';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
@@ -89,13 +93,17 @@ function display(canvas, arr, swappedIndex, numSwaps, name) {
         const rectY = canvas.height - (arr[i] / max * canvas.height)
         const rectWidth  = 5;
         const rectHeight = (arr[i] / max * canvas.height);
-        ctx.fillStyle = i == swappedIndex ? 'blue': 'green';
+        ctx.fillStyle = i == swappedIndex ? 'red': 'green';
         ctx.fillRect(rectX, rectY, rectWidth, rectHeight);
     }
-
+    ctx.fillStyle = 'white'
     ctx.font = '20px serif';
-    ctx.fillText(name,  10, 30);
+    ctx.fillText(name + " " + getSeconds(timeElapsed) + "s",  10, 30);
     ctx.fillText(`Number of swaps: ${numSwaps}`, 10, 50);
+}
+
+function getSeconds(ms) {
+    return (ms / 1000).toFixed(4);
 }
 
 function finished() {
